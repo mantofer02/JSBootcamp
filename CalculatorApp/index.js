f4709071ae579160baa751310e3aa95dff0d5770
Marco Ferraro <mantofer2000@gmail.com>
2022-03-08 12:14:13 -0600

    Basic Calculator App

--- CalculatorApp/index.js
+++ CalculatorApp/index.js
@@ -1,48 +1,57 @@
-
 // camelCase for JS variables
 
-let operandA = 0
-let operandB = 0
-let operator = ""
-let textEl = document.getElementById("text-el")
+let operandA = 0;
+let operandB = 0;
+let operator = "";
+let textEl = document.getElementById("text-el");
 
-function writeOne() {
-  textEl.innerText += "1"
-}
-
-function writeTwo() {
-  textEl.innerText += "2"
-}
-
-function writeThree() {
-  textEl.innerText += "3"
-}
-function writeFour() {
-  textEl.innerText += "4"
-}
-
-function writeFive() {
-  textEl.innerText += "5"
-}
-
-function writeSix() {
-  textEl.innerText += "6"
-}
-
-function writeSeven() {
-  textEl.innerText += "7"
-}
-
-function writeEight() {
-  textEl.innerText += "8"
-}
-
-function writeNine() {
-  textEl.innerText += "9"
-}
-
-function writeZero() {
-  if (!(textEl.innerText === "")) {
-    textEl.innerText += "0"
+function writeDigit(n) {
+  if (n == 0 && textEl.innerText == "") {
+    return;
+  } else {
+    textEl.innerText += n;
   }
+}
+
+function writeOp(op) {
+  if (!(textEl.innerText == "")) {
+    operator = op;
+    operandA = parseInt(textEl.innerText);
+    textEl.innerText = "";
+  }
+}
+
+function result() {
+  n = 0;
+  switch (operator) {
+    case "+":
+      n = operandA + operandB;
+      break;
+    case "-":
+      n = operandA - operandB;
+      break;
+    case "*":
+      n = operandA * operandB;
+      break;
+    case "/":
+      n = operandA / operandB;
+      break;
+    default:
+      break;
+  }
+  return n;
+}
+
+function equals() {
+  if (!(operator === "" && textEl === "")) {
+    operandB = parseInt(textEl.innerText);
+    textEl.innerText = result();
+  }
+}
+
+function clearText() {
+  operandA = 0;
+  operandB = 0;
+  operator = "";
+  textEl.innerText = "";
 }
